b0bb55cc1f3e4d081131a6783eabd1eec1cff951
Egor <Cost_benefit@mail.ru>
2021-11-05 22:35:13 +0300

    выполнил вставку дом элементов в цикле по клику - верхнего меню и только верхнего

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ shop.onclick = () => {document.location.href = 'shop-empty.html'}
 
 //лендинг
 const menu = document.querySelectorAll('.choose-elem');
+
 const buttonMenu = document.querySelectorAll('.add-btn');
 const delMenu = () => {
 	menu.forEach(item => {
@@ -34,10 +35,100 @@ buttonMenu.forEach((item, index) => {
 });
 
 window.onclick = (e) => {
-	console.log(e.target.tagName)
 		if (!e.target.closest('.add-btn')) {
 			delMenu();
 		}
 }
-wadawdadad
-asfasffasf11111
+const massMenu = ['Заголовок H1', 'Заголовок H2', 'Заголовок H3', 'Абзац текста', 'Изображение'];
+//Добавляем заголовок в header
+const content = document.querySelector('.content');
+
+const ggg = (index) => {
+	const header = document.querySelector('.header');
+	const fragment = new DocumentFragment();
+	const div = document.createElement('div');
+	const divv = div.cloneNode();
+	const h1 = document.createElement('h1');
+	const button = document.createElement('button');
+	const span = document.createElement('span');
+
+	span.classList.add('visually-hidden');
+	span.innerHTML = 'Удалить элемент';
+	button.setAttribute('type', 'button');
+	button.classList.add('delete-btn');
+	h1.setAttribute('data-placeholder', massMenu[index]);
+	h1.setAttribute('contenteditable', 'true');
+	h1.innerHTML = massMenu[index];
+	div.classList.add('header__elements-wrapper');
+	divv.classList.add('element', 'title');
+	divv.setAttribute('tabindex', '0');
+
+	header.append(div);
+	div.append(divv);
+	divv.append(h1);
+	divv.append(button);
+	button.append(span);
+};
+const ggg1 = (index) => {
+	const header = document.querySelector('.header');
+	const fragment = new DocumentFragment();
+	const div = document.createElement('div');
+	const divv = div.cloneNode();
+	const h1 = document.createElement('h1');
+	const button = document.createElement('button');
+	const span = document.createElement('span');
+
+	span.classList.add('visually-hidden');
+	span.innerHTML = 'Удалить элемент';
+	button.setAttribute('type', 'button');
+	button.classList.add('delete-btn');
+	h1.setAttribute('data-placeholder', massMenu[index]);
+	h1.setAttribute('contenteditable', 'true');
+	h1.innerHTML = massMenu[index];
+	div.classList.add('header__elements-wrapper');
+	divv.classList.add('element', 'title');
+	divv.setAttribute('tabindex', '0');
+
+	header.append(div);
+	div.append(divv);
+	divv.append(h1);
+	divv.append(button);
+	button.append(span);
+};
+const ggg2 = (index) => {
+	const header = document.querySelector('.header');
+	const fragment = new DocumentFragment();
+	const div = document.createElement('div');
+	const divv = div.cloneNode();
+	const h1 = document.createElement('h1');
+	const button = document.createElement('button');
+	const span = document.createElement('span');
+
+	span.classList.add('visually-hidden');
+	span.innerHTML = 'Удалить элемент';
+	button.setAttribute('type', 'button');
+	button.classList.add('delete-btn');
+	h1.setAttribute('data-placeholder', massMenu[index]);
+	h1.setAttribute('contenteditable', 'true');
+	h1.innerHTML = massMenu[index];
+	div.classList.add('header__elements-wrapper');
+	divv.classList.add('element', 'title');
+	divv.setAttribute('tabindex', '0');
+
+	header.append(div);
+	div.append(divv);
+	divv.append(h1);
+	divv.append(button);
+	button.append(span);
+};
+menu.forEach(item => {
+	const buttonMenuu = item.querySelectorAll('button');
+	for (let i = 0; i<buttonMenuu.length; i++) {
+	buttonMenuu[i].onclick = () => {
+		ggg(i);
+	}
+}
+})
+
+
+
