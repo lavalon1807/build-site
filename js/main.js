cd98fbe0c4dd502ed20858ffbe69b239fbfbbc7a
Egor <Cost_benefit@mail.ru>
2021-11-06 23:50:07 +0300

    с помощью переменных и метода splice создал массив с помощью которого открываю меню во всех вкладках

--- js/main.js
+++ js/main.js
@@ -41,11 +41,19 @@ window.onclick = (e) => {
 }
 const massMenu = ['Заголовок H1', 'Заголовок H2', 'Заголовок H3', 'Абзац текста', 'Изображение'];
 //Добавляем заголовок в header
-const content = document.querySelector('.content');
+const header = document.querySelector('.header');
+const content = document.querySelectorAll('.content');
+const footer = document.querySelector('.footer');
+const massCCC = [
+	header,  
+	footer
+];
+console.log(massCCC);
+for(let i =0; i<content.length; i++) {
+	massCCC.splice(-1, 0, content[i]);
+}
 
-const ggg = (index) => {
-	const header = document.querySelector('.header');
-	const fragment = new DocumentFragment();
+const ggg = (index, count) => {
 	const div = document.createElement('div');
 	const divv = div.cloneNode();
 	const h1 = document.createElement('h1');
@@ -62,73 +70,19 @@ const ggg = (index) => {
 	div.classList.add('header__elements-wrapper');
 	divv.classList.add('element', 'title');
 	divv.setAttribute('tabindex', '0');
-
-	header.append(div);
+		//Меняем переменые, которые размещают дом элементы(место их вставки)
+	massCCC[count].append(div);
 	div.append(divv);
 	divv.append(h1);
 	divv.append(button);
 	button.append(span);
 };
-const ggg1 = (index) => {
-	const header = document.querySelector('.header');
-	const fragment = new DocumentFragment();
-	const div = document.createElement('div');
-	const divv = div.cloneNode();
-	const h1 = document.createElement('h1');
-	const button = document.createElement('button');
-	const span = document.createElement('span');
 
-	span.classList.add('visually-hidden');
-	span.innerHTML = 'Удалить элемент';
-	button.setAttribute('type', 'button');
-	button.classList.add('delete-btn');
-	h1.setAttribute('data-placeholder', massMenu[index]);
-	h1.setAttribute('contenteditable', 'true');
-	h1.innerHTML = massMenu[index];
-	div.classList.add('header__elements-wrapper');
-	divv.classList.add('element', 'title');
-	divv.setAttribute('tabindex', '0');
-
-	header.append(div);
-	div.append(divv);
-	divv.append(h1);
-	divv.append(button);
-	button.append(span);
-};
-const ggg2 = (index) => {
-	const header = document.querySelector('.header');
-	const fragment = new DocumentFragment();
-	const div = document.createElement('div');
-	const divv = div.cloneNode();
-	const h1 = document.createElement('h1');
-	const button = document.createElement('button');
-	const span = document.createElement('span');
-
-	span.classList.add('visually-hidden');
-	span.innerHTML = 'Удалить элемент';
-	button.setAttribute('type', 'button');
-	button.classList.add('delete-btn');
-	h1.setAttribute('data-placeholder', massMenu[index]);
-	h1.setAttribute('contenteditable', 'true');
-	h1.innerHTML = massMenu[index];
-	div.classList.add('header__elements-wrapper');
-	divv.classList.add('element', 'title');
-	divv.setAttribute('tabindex', '0');
-
-	header.append(div);
-	div.append(divv);
-	divv.append(h1);
-	divv.append(button);
-	button.append(span);
-};
-menu.forEach(item => {
+menu.forEach((item, index) => {
 	const buttonMenuu = item.querySelectorAll('button');
 	for (let i = 0; i<buttonMenuu.length; i++) {
 	buttonMenuu[i].onclick = () => {
-		ggg(i);
+		ggg(i, index);
 	}
 }
 })
-
-
-
