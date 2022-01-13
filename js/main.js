b156562bf04432e45cfa7f48a47364efd9880caa
Egor <Cost_benefit@mail.ru>
2022-01-13 13:59:23 +0300

    выполнил добавление контента изображения и загрузки фото с помощью адресной строки

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ window.onclick = (e) => {
 		}
 }
 const massMenu = ['Заголовок H1', 'Заголовок H2', 'Заголовок H3', 'Абзац текста', 'Изображение'];
-const massText = ['title', 'title', 'title', 'text', 'title'];
+const massText = ['title', 'title', 'title', 'text', 'image'];
 //Добавляем заголовок в header
 const header = document.querySelector('.header');
 const content = document.querySelectorAll('.content');
@@ -77,7 +77,7 @@ const constructorMenu = (index, count) => {
 	const wrapper = masContents[count].querySelector('.' + masContentsElement[count]);
 
 	const div = document.createElement('div');
-	const h1 = document.createElement(massMenuElement[index]);
+	const h = document.createElement(massMenuElement[index]);
 	const button = document.createElement('button');
 	const span = document.createElement('span');
 
@@ -85,30 +85,44 @@ const constructorMenu = (index, count) => {
 	span.innerHTML = 'Удалить элемент';
 	button.setAttribute('type', 'button');
 	button.classList.add('delete-btn');
-	h1.setAttribute('data-placeholder', massMenu[index]);
-	h1.setAttribute('contenteditable', 'true');
-	h1.innerHTML = massMenu[index];
+	h.setAttribute('data-placeholder', massMenu[index]);
+	h.setAttribute('contenteditable', 'true');
+	h.innerHTML = massMenu[index];
 	div.classList.add('element', massText[index]);
 	masContents[count].classList.remove(masEmptyWrapper[count]);
 
 	div.setAttribute('tabindex', '0');
 		//Меняем переменые, которые размещают дом элементы(место их вставки)
 	wrapper.append(div);
-	div.append(h1);
+	div.append(h);
 	div.append(button);
 	button.append(span);
 };
 //вставляем в разметку svg и path
-const constructorMenuImage = (count) => {
+const constructorMenuImage = (index, count) => {
+	const wrapper = masContents[count].querySelector('.' + masContentsElement[count]);
+	const div = document.createElement('div');
+  const buttonImg = document.createElement('button');
   const button = document.createElement('button');
+  const span = document.createElement('span');
+  
+
+  buttonImg.setAttribute('type', 'button');
+  buttonImg.classList.add('add-img-btn');
   button.setAttribute('type', 'button');
-  button.classList.add('add-img-btn');
+	button.classList.add('delete-btn');
+	
+  span.classList.add('visually-hidden');
+	span.innerHTML = 'Удалить элемент';
  
+
+  div.classList.add('element', 'element--' + massText[index], massText[index]);
+  div.setAttribute('tabindex', '0');
+  masContents[count].classList.remove(masEmptyWrapper[count]);
 
   const xmlns = 'http://www.w3.org/2000/svg';
   const boxWidth = 32;
   const boxHeight = 32;
-  const addBoxSize = 16;
 
   const svg = document.createElementNS(xmlns, 'svg');
   svg.setAttributeNS(null, 'width', boxWidth);
@@ -120,12 +134,63 @@ const constructorMenuImage = (count) => {
 	newpath.setAttributeNS(null, "fill-rule", "evenodd");
 	newpath.setAttributeNS(null, "clip-rule", "evenodd"); 
 	newpath.setAttributeNS(null, "d", "M6 2V8H0V12H6V18H10V12H16V8H10V2H6ZM12 14V20H6V40C6 42.2 7.8 44 10 44H42C44.2 44 46 42.2 46 40V16C46 13.8 44.2 12 42 12H35.66L32 8H18V14H12ZM26 38C31.52 38 36 33.52 36 28C36 22.48 31.52 18 26 18C20.48 18 16 22.48 16 28C16 33.52 20.48 38 26 38ZM26 34C22.68 34 20 31.32 20 28C20 24.68 22.68 22 26 22C29.32 22 32 24.68 32 28C32 31.32 29.32 34 26 34Z");
-
+	
 	svg.append(newpath);
-	button.append(svg);
-  header.appendChild(button);
+	buttonImg.append(svg);
+	button.append(span);
+	div.append(button);
+  div.append(buttonImg);
+  
+	wrapper.append(div);
+
+	buttonImg.addEventListener('click', () => {
+		constructorUploadImage(div, buttonImg)
+	}, {once: true});
 };
-constructorMenuImage();
+
+const constructorUploadImage = (divWrapper, buttonLoadPicture) => {
+	const divLoad = document.createElement('div');
+	const pLoad = document.createElement('p');
+	const input = document.createElement('input');
+  const label = document.createElement('label');
+  const inputLabel = document.createElement('input');
+  const img = document.createElement('img');
+
+  img.setAttribute('src', '');
+
+  divWrapper.classList.add('element--uploading');
+
+  inputLabel.classList.add('visually-hidden');
+  inputLabel.setAttribute('type', 'file');
+  inputLabel.setAttribute('accept', 'image/png, image/jpeg');
+
+  divLoad.classList.add('img-upload');
+	label.classList.add('img-upload__label');
+	label.textContent = 'Загрузить';
+	
+	divLoad.style.display = 'block';
+	
+	pLoad.textContent = 'Загрузите изображение';
+	input.setAttribute('type', 'url');
+	input.setAttribute('placeholder', 'Вставить ссылку на изображение');
+
+	divWrapper.append(divLoad);
+	divLoad.append(pLoad);
+	divLoad.append(input);
+	divLoad.append(label);
+	label.append(inputLabel);
+
+
+	label.onclick = (evt) => {
+		divWrapper.append(img);
+		divLoad.style.display = 'none';
+		buttonLoadPicture.style.display = 'none';
+		divWrapper.classList.remove('element--image');
+		const file = input.value;
+		img.src = file;
+		evt.preventDefault();
+	}
+}
 
 menu.forEach((item, index) => {
 	item.onclick = (evt) => {
@@ -136,11 +201,13 @@ menu.forEach((item, index) => {
 						if (!wrapper) {
 							if (menu[index].style.display == 'flex') {
 								constructorMenuWrapper(index);
-								constructorMenu(i, index);
+								// constructorMenu(i, index);
+								constructorMenuImage(i, index);
 								delElement(index);
 							}
 					}	else if (menu[index].style.display == 'flex') {
-						constructorMenu(i, index);
+						// constructorMenu(i, index);
+						constructorMenuImage(i, index);
 						delElement(index);
 				} 
 			}			
